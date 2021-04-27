1ac1a4bb55212d2fde79ef6cf414b20f1790d69b
maya <maya.salcedo07@gmail.com>
2021-04-27 22:02:05 +0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import data from './data';
+import Product from './components/Product';
+
+const App = () => {
+  return (
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="/">
+            escooterlane shop
+          </a>
+        </div>
+        <div>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
+        </div>
+      </header>
+      <main>
+        <div>
+          <div className="row center">
+            {data.products.map((product) => (
+              <Product key={product._id} product={product}></Product>
+            ))}
+          </div>
+        </div>
+      </main>
+      <footer className="row center">All right reserved</footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Product.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Rating from './Rating';
+
+const Product = (props) => {
+  const { product } = props;
+  return(
+    <div key={product._id} className="card">
+      <a href={`/product/${product._id}`}>
+        <img
+          className="medium"
+          src={product.image}
+          alt={product.name}
+        />
+      </a>
+      <div className="card-body">
+        <a href={`/product/${product._id}`}>
+          <h2>{product.name}</h2>
+        </a>
+        <Rating rating={product.rating} numReviews={product.numReviews}/>
+        <div className="price">€{product.price}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Product;
