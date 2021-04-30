fe0772ffaabe68f232fda7eb92fb9d8ea98d5a93
maya <maya.salcedo07@gmail.com>
2021-04-30 12:01:25 +0300

    added redux-thunk

--- client/src/store.js
+++ client/src/store.js
@@ -1,4 +1,5 @@
-import { createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
+import thunk from 'redux-thunk';
 import data from './data';
 
 const initialState = {};
@@ -7,6 +8,6 @@ const reducer = (state, action) => {
   return { products: data.products };
 };
 
-const store = createStore(reducer, initialState);
+const store = createStore(reducer, initialState, compose(applyMiddleware(thunk)));
 
 export default store;
