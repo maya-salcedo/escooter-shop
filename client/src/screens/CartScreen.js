f445ed27dba44d1f758bf7db28b49aabf5ad68d2
maya <maya.salcedo07@gmail.com>
2021-05-03 15:11:43 +0300

    implemented remove from cart action

--- client/src/screens/CartScreen.js
+++ client/src/screens/CartScreen.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { addToCart } from '../actions/cartActions';
+import { addToCart, removeFromCart } from '../actions/cartActions';
 import MessageBox from '../components/MessageBox';
 
 const CartScreen = (props) => {
@@ -20,7 +20,7 @@ const CartScreen = (props) => {
   }, [dispatch, productId, qty]);
 
   const removeFromCartHandler = (id) => {
-    //delete action
+    dispatch(removeFromCart(id));
   };
 
   const checkoutHandler = () => {
