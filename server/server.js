fca34ca47c0e14b14bf0dd641469fbb31ed89700
maya <maya.salcedo07@gmail.com>
2021-05-03 20:56:06 +0300

    added middleware to catch error and send back to frontend

--- server/server.js
+++ server/server.js
@@ -32,7 +32,9 @@ app.get('/', (req, res) => {
   res.send('Server is ready');
 });
 
-// app.use((err, req, res, next))
+app.use((err, req, res, next) => {
+  res.status(500).send({message: err.message});
+});
 
 const port = process.env.PORT || 5000;
 
