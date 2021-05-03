81b9aba671f9f2bd9b36d27a54427469653175d4
maya <maya.salcedo07@gmail.com>
2021-05-03 21:00:22 +0300

    addded a line to remove existing user before creating new users

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -7,6 +7,7 @@ const userRouter = express.Router();
 
 userRouter.get('/seed', 
   expressAsyncHandler(async (req, res) => {
+    // await User.remove({}); //to remove existing user before creating new users
     const createdUsers = await User.insertMany(data.users);
     res.send({ createdUsers });
   })
