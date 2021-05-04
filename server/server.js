b6474f6f8fd5174ef6cc42ea0816d23712961bc5
maya <maya.salcedo07@gmail.com>
2021-05-04 14:49:38 +0300

    inserted product details into mongodb

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
 import express from 'express'; 
-import data from './data.js';
 import mongoose from 'mongoose';
 import userRouter from './routers/userRouter.js';
-
+import productRouter from './routers/productRouter.js';
 
 const app = express(); 
 
@@ -13,20 +12,8 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/escooterlanesho
 });
 
 
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({message: 'Product not Found'})
-  }
-});
-
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
-});
-
 app.use('/api/users', userRouter);
+app.use('/api/products', productRouter); //to send data from mongodb
 
 app.get('/', (req, res) => {
   res.send('Server is ready');
