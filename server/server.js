e2eb4944bd4b79b0cacbc5cff0f5c29807519a96
maya <maya.salcedo07@gmail.com>
2021-05-05 20:27:56 +0300

    created api for signin

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 import express from 'express'; 
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import userRouter from './routers/userRouter.js';
 import productRouter from './routers/productRouter.js';
 
+dotenv.config();
+
 const app = express(); 
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/escooterlaneshop', {
   useNewUrlParser: true,
