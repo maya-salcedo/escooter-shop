360062af6692347f3ab7919cdceb50a36f0ce32f
maya <maya.salcedo07@gmail.com>
2021-05-09 20:30:25 +0300

    implemented error message to  invalid signin password

--- client/src/screens/SigninScreen.js
+++ client/src/screens/SigninScreen.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { signin } from '../actions/userActions';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 const SigninScreen = (props) => {
 
@@ -13,7 +15,7 @@ const SigninScreen = (props) => {
     : '/';
 
   const userSignin = useSelector((state) => state.userSignin);
-  const { userInfo } = userSignin;
+  const { userInfo, loading, error } = userSignin;
 
   const dispatch = useDispatch();
   const submitHandler = (e) => {
@@ -32,6 +34,8 @@ const SigninScreen = (props) => {
         <div>
           <h1>Sign In</h1>
         </div>
+        {loading && <LoadingBox></LoadingBox>}
+        {error && <MessageBox variant="danger">{error}</MessageBox>}
         <div>
           <label htmlFor="email">Email</label>
           <input type="email" id="email" placeholder="Enter email" required onChange={e => setEmail(e.target.value)} />
