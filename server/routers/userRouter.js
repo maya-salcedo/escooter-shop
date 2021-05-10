8d32394bf362799216194b6a4d43720c03e8a731
maya <maya.salcedo07@gmail.com>
2021-05-10 20:25:51 +0300

    create API for /api/users/register

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -34,4 +34,22 @@ userRouter.post('/signin',
   })
 );
 
+userRouter.post('/register',
+  expressAsyncHandler(async (req, res) => {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 8)
+    });
+    const createUser = await user.save();
+    res.send({
+      _id: createUser._id,
+      name: createUser.name,
+      email: createUser.email,
+      idAdmin: createUser.isAdmin,
+      token: generateToken(createUser)
+    });
+  })
+);
+
 export default userRouter;
