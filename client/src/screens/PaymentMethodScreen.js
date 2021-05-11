e79e747c834c0a7bdc6fab34ea889ceb8d702dd1
maya <maya.salcedo07@gmail.com>
2021-05-11 22:34:44 +0300

    redirected to shipping screen if shipping details are not available

--- client/src/screens/PaymentMethodScreen.js
+++ client/src/screens/PaymentMethodScreen.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { savePaymentMethod } from '../actions/cartActions';
 import CheckoutSteps from '../components/CheckoutSteps';
 
 const PaymentMethodScreen = (props) => {
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+  if (!shippingAddress.address) {
+    props.history.push('/shipping');
+  }
   const [paymentMethod, setPaymentMethod] = useState('PayPal');
   const dispatch = useDispatch;
   const submitHandler = (e) => {
