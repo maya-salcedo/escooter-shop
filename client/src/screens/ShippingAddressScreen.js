a925a3a7cdb35f3597d7ef028182d7dc1c8a3118
maya <maya.salcedo07@gmail.com>
2021-05-11 13:15:16 +0300

    created and styled CheckoutSteps.js component

---
+++ client/src/screens/ShippingAddressScreen.js
@@ -0,0 +1,11 @@
+import CheckoutSteps from "../components/CheckoutSteps"
+
+const ShippingAddressScreen = () => {
+  return(
+    <div>
+      <CheckoutSteps step1 step2></CheckoutSteps>
+    </div>
+  );
+};
+
+export default ShippingAddressScreen;
