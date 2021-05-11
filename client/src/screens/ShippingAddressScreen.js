8b90fe41e96127196c83cc5310f7193a14db113c
maya <maya.salcedo07@gmail.com>
2021-05-11 21:48:04 +0300

    saved the input's data in the localStorage

--- client/src/screens/ShippingAddressScreen.js
+++ client/src/screens/ShippingAddressScreen.js
@@ -1,20 +1,26 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { saveShippingAddress } from "../actions/cartActions";
 import CheckoutSteps from "../components/CheckoutSteps"
 
 const ShippingAddressScreen = (props) => {
-  const [fullName, setFullName] = useState('');
-  const [address, setAddress] = useState('');
-  const [city, setCity] = useState('');
-  const [postalCode, setPostalCode] = useState('');
-  const [country, setCountry] = useState('');
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  const cart = useSelector(state => state.cart);
+  const { shippingAddress } = cart;
+  if (!userInfo){
+    props.history.push('/signin');
+  }
+  const [fullName, setFullName] = useState(shippingAddress.fullName);
+  const [address, setAddress] = useState(shippingAddress.address);
+  const [city, setCity] = useState(shippingAddress.city);
+  const [postalCode, setPostalCode] = useState(shippingAddress.postalCode);
+  const [country, setCountry] = useState(shippingAddress.country);
   const dispatch = useDispatch();
   const submitHandler = (e) => {
     e.preventDefault();
     dispatch(saveShippingAddress({fullName, address, city, postalCode, country}));
     props.history.push('/payment');
-    //To Do: dispatch save shipping address
   }
   return(
     <div>
