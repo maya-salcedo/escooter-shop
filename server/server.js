4f019901437831cbd5f7479535ad468f0a859a4f
maya <maya.salcedo07@gmail.com>
2021-05-20 14:27:05 +0300

    added upload product image feature

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 import express from 'express'; 
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import path from 'path';
 import userRouter from './routers/userRouter.js';
 import productRouter from './routers/productRouter.js';
 import orderRouter from './routers/orderRouter.js';
+import uploadRouter from './routers/uploadRouter.js';
 
 dotenv.config();
 
@@ -18,13 +20,15 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/escooterlanesho
   useCreateIndex: true
 });
 
-
+app.use('/api/uploads', uploadRouter);
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter); //to send data from mongodb
 app.use('/api/orders', orderRouter); 
 app.use('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 app.get('/', (req, res) => {
   res.send('Server is ready');
 });
