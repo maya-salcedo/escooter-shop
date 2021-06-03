e18c6e0696e2adc4e251028fdd25453c3895c751
maya <maya.salcedo07@gmail.com>
2021-06-03 20:57:06 +0300

    not working yet

--- server/routers/uploadRouter.js
+++ server/routers/uploadRouter.js
@@ -1,6 +1,8 @@
 import multer from 'multer';
 import express from 'express';
-import { isAuth } from '../utils.js'
+import { isAuth } from '../utils.js';
+import multerS3 from 'multer-s3';
+import aws from 'aws-sdk';
 
 const uploadRouter = express.Router();
 
@@ -17,6 +19,26 @@ const upload = multer({storage}); //middleware
 
 uploadRouter.post('/', isAuth, upload.single('image'), (req, res) => {
   res.send(`/${req.file.path}`);
+});
+
+aws.config.update({
+  accessKeyId: config.accessKeyId,
+  secretAccessKey: config.secretAccessKey
+});
+
+const s3 = new aws.S3();
+const storageS3 =multerS3({
+  s3,
+  bucket: 'escooter-shop-bucket',
+  acl: 'public-read',
+  contentType: multerS3.AUTO_CONTENT_TYPE,
+  key(req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+const uploadS3 = multer({ storage: storageS3 });
+router.post('/s3', uploadS3.single('image'), (req, res) => {
+  res.send(req.file.location);
 });
 
 export default uploadRouter;
