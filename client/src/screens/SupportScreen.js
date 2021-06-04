381a348d3466832acc1537cdc2c782bc453b432d
maya <maya.salcedo07@gmail.com>
2021-06-04 12:46:16 +0300

    added state to admin chat screen

---
+++ client/src/screens/SupportScreen.js
@@ -0,0 +1,141 @@
+import React, { useEffect, useState } from 'react';
+import socketIOClient from 'socket.io-client';
+import { useSelector } from 'react-redux';
+import MessageBox from '../components/MessageBox';
+import { skipPartiallyEmittedExpressions } from '../../node_modules/typescript/lib/typescript';
+
+let allUsers = [];
+let allMessages = [];
+let allSelectedUser = {};
+const ENDPOINT = 
+  window.location.host.indexOf('localhost') >= 0
+    ? 'http://127.0.0.1:5000'
+    : window.location.host;
+
+const SupportScreen = () => {
+  const [selectedUser, setSelectedUser] = useState({});
+  const [ socket, setSocket] = useState(null);
+  const uiMessagesRef = useRef(null);
+  const [messageBody, setMessageBody] = userState('');
+  const [messages, setMessages] = useState([]);
+  const [users, setUsers] = useState([]);
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  
+  useEffect(() => {
+    if (uiMessagesRef.current) {
+      uiMessagesRef.current.scrollBy({
+        top: uiMessagesRef.current.clientHeight,
+        left: 0,
+        behavior: smooth
+      });
+      if (!socket) {
+        const sk = socketIOClient(ENDPOINT);
+        setSocket(sk);
+      }
+      sk.emit('onLogin', {
+        _id: userInfo._id,
+        name: userInfo.name,
+        isAdmin: userInfo.isAdmin
+      });
+      sk.on('message', (data) => {
+        if (allSelectedUser._id === data._id) {
+          allMessages = [...allMessages, data];
+        } else {
+          const existUser = allUsers.find((user) => user._id === data._id);
+          if (existUser) {
+            allUsers = allUsers.map((user) => 
+            user._id === existUser._id ? { ...user, unread: true } : user 
+            );
+            setUsers(allUsers);
+          }
+        }
+        setMessages(allMessages);
+      });
+      sk.on('updateUser', (updatedUser) => {
+        const existUser = allUsers.find((user) => user._id === updatedUser._id);
+        if (existUser) {
+          allUsers = allUsers.map((user) => 
+          user._id === existUser._id ? updatedUser : user
+          );
+          setUsers(allUsers);
+        } else {
+          allUsers = [...allUsers, updatedUser];
+          setUsers(allUsers);
+        }
+      });
+      sk.on('listUsers', (updatedUser) => {
+        allUsers = updatedUser;
+        setUsers(allUsers);
+      });
+      sk.on('selectUser', (user) => {
+        allMessages = user.messages;
+        setMessages(allMessages);
+      })
+    }
+    
+  }, [messages, socket, users]);
+  return (
+    <div className="row top full-container">
+      <div className="col-1 support-users">
+        {users.filter((x) => x._id !== userInfo._id).length === 0 && (
+          <MessageBox>No Online User Found</MessageBox>
+        )}
+        <ul>
+          {users
+            .filter((x) => x._id !== userInfo._id)
+            .map((user) => (
+              <li
+                key={user._id}
+                className={user._id === selectedUser._id ? ' selected' : ' ' }
+              >
+                <button
+                  className="block"
+                  type="button"
+                  onClick={() => selectedUser(user)}
+                >
+                 {user.name}
+                </button>
+                <span
+                  className={user.unread ? 'unread' : user.online ? 'online' : 'offline' }
+                />
+              </li>
+            ))}
+        </ul>
+      </div>
+      <div className="col-3 support-messages">
+        {!selectedUser._id ? (
+          <MessageBox>Select a user to start a chat</MessageBox>
+        ) : (
+          <div>
+            <div className="row">
+            <strong>Chat with {selectedUser.name}</strong>
+            </div>
+            <ul ref={uiMessagesRef}>
+            {messages.length === 0 && <li>No message.</li>}
+            {messages.map((msg, index) => (
+              <li key={index}>
+                <strong>{`${msg.name}:`}</strong> {msg.body}
+              </li>
+            ))}
+            </ul>
+            <div>
+              <form onSubmit={submitHandler} className="row">
+                <input
+                  value={messageBody}
+                  onChange={(e) => setMessageBody(e.target.value)}
+                  type="text"
+                  placeholder="type message"
+                >
+                </input>
+                <button type="submit">Send</button>
+              </form>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default SupportScreen;
