78d2116f8a77ecb73bf7a150cf8161e9f8c70634
maya <maya.salcedo07@gmail.com>
2021-08-02 11:07:53 +0300

    desktop view shows normal table, mobile view show vertical style table

--- client/src/screens/ProductListScreen.js
+++ client/src/screens/ProductListScreen.js
@@ -96,47 +96,51 @@ const ProductListScreen = (props) => {
         <MessageBox variant="danger">{error}</MessageBox>
       ) : (
         <>
-          <table className="table">
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>NAME</th>
-                <th>PRICE</th>
-                <th>CATEGORY</th>
-                <th>BRAND</th>
-                <th>ACTIONS</th>
-              </tr>
-            </thead>
-            <tbody>
-              {products.map((product) => (
-                <tr key={product._id}>
-                  <td>{product._id}</td>
-                  <td>{product.name}</td>
-                  <td>{product.price}</td>
-                  <td>{product.category}</td>
-                  <td>{product.brand}</td>
-                  <td>
-                    <button
-                      type="button"
-                      className="small"
-                      onClick={() =>
-                        props.history.push(`/product/${product._id}/edit`)
-                      }
-                    >
-                      Edit
-                    </button>
-                    <button
-                      type="button"
-                      className="small"
-                      onClick={() => deleteHandler(product)}
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          {!isMobile && (
+            <>
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>NAME</th>
+                    <th>PRICE</th>
+                    <th>CATEGORY</th>
+                    <th>BRAND</th>
+                    <th>ACTIONS</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {products.map((product) => (
+                    <tr key={product._id}>
+                      <td>{product._id}</td>
+                      <td>{product.name}</td>
+                      <td>{product.price}</td>
+                      <td>{product.category}</td>
+                      <td>{product.brand}</td>
+                      <td>
+                        <button
+                          type="button"
+                          className="small"
+                          onClick={() =>
+                            props.history.push(`/product/${product._id}/edit`)
+                          }
+                        >
+                          Edit
+                        </button>
+                        <button
+                          type="button"
+                          className="small"
+                          onClick={() => deleteHandler(product)}
+                        >
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </>
+          )}
           {isMobile && (
             <>
               {products.map((product) => (
