09d7c84c5f79fb37bd64f34ba0274a7438ce7049
maya <maya.salcedo07@gmail.com>
2021-08-03 22:26:12 +0300

    added styled component to the product card

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,25 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Rating from './Rating';
+import styled from 'styled-components';
+
+const ContainerWrapper = styled.div`
+  border: 0.1rem #c0c0c0 solid;
+  background-color: #f8f8f8;
+  border-radius: 0.5rem;
+  margin: 1rem;
+  > div {
+    padding: 1rem;
+  }
+`;
+
+const ImageWrapper = styled.img`
+  max-width: 29rem;
+  width: 100%;
+`;
+
+const PriceWrapper = styled.div`
+  font-size: 2rem;
+`;
 
 const Product = (props) => {
   const { product } = props;
-  return(
-    <div key={product._id} className="card">
+  return (
+    <ContainerWrapper key={product._id}>
       <Link to={`/product/${product._id}`}>
-        <img
-          className="medium"
-          src={product.image}
-          alt={product.name}
-        />
+        <ImageWrapper src={product.image} alt={product.name} />
       </Link>
-      <div className="card-body">
+      <div>
         <Link to={`/product/${product._id}`}>
           <h2>{product.name}</h2>
         </Link>
-        <Rating rating={product.rating} numReviews={product.numReviews}/>
+        <Rating rating={product.rating} numReviews={product.numReviews} />
         <div>
-          <div className="price">€{product.price}</div>
+          <PriceWrapper>€{product.price}</PriceWrapper>
           <div>
             <Link to={`/seller/${product.seller._id}`}>
               {product.seller.seller.name}
@@ -27,8 +43,8 @@ const Product = (props) => {
           </div>
         </div>
       </div>
-    </div>
+    </ContainerWrapper>
   );
-}
+};
 
 export default Product;
