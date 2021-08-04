66a6caa95125747647e7c07064aa4ac31828cb12
maya <maya.salcedo07@gmail.com>
2021-08-04 20:19:29 +0300

    styled component added to home screen

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import styled from 'styled-components';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Carousel } from 'react-responsive-carousel';
 import Product from '../components/Product';
@@ -9,58 +10,71 @@ import { listProducts } from '../actions/productActions';
 import { listTopSellers } from '../actions/userActions';
 import { Link } from 'react-router-dom';
 
+const ProductWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  justify-content: center;
+`;
+
 const HomeScreen = () => {
   const dispatch = useDispatch();
-  
+
   const productList = useSelector((state) => state.productList);
   const { loading, error, products } = productList;
 
   const userTopSellersList = useSelector((state) => state.userTopSellersList);
-  const { loading: loadingSellers, error: errorSellers, users: sellers } = userTopSellersList;
+  const {
+    loading: loadingSellers,
+    error: errorSellers,
+    users: sellers,
+  } = userTopSellersList;
 
-  useEffect(() =>{
+  useEffect(() => {
     dispatch(listProducts({}));
     dispatch(listTopSellers());
   }, [dispatch]);
 
-  return(
+  return (
     <div>
-      <h2>Top Sellers</h2>      
-        {loadingSellers ? (
-            <LoadingBox></LoadingBox>
-          ) : errorSellers ? (
-            <MessageBox variant="danger">{error}</MessageBox>
-          ) : (
-            <>
-            {sellers.length === 0 && <MessageBox>No Sellers Found</MessageBox>}
-            <Carousel showArrows autoplay showThumbs={false}>
-              {sellers.map((seller) => (
-                <div key={seller._id}>
-                  <Link to={`/seller/${seller._id}`}>
-                    <img src={seller.seller.logo} alt={seller.seller.name} />
-                    <p className="legend">{seller.seller.name}</p>
-                  </Link>
-                </div>
-              ))}
-            </Carousel>
-            </>
-          )}  
+      <h2>Top Sellers</h2>
+      {loadingSellers ? (
+        <LoadingBox></LoadingBox>
+      ) : errorSellers ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <>
+          {sellers.length === 0 && <MessageBox>No Sellers Found</MessageBox>}
+          <Carousel showArrows autoplay showThumbs={false}>
+            {sellers.map((seller) => (
+              <div key={seller._id}>
+                <Link to={`/seller/${seller._id}`}>
+                  <img src={seller.seller.logo} alt={seller.seller.name} />
+                  <p className="legend">{seller.seller.name}</p>
+                </Link>
+              </div>
+            ))}
+          </Carousel>
+        </>
+      )}
       <h2>Featured Products</h2>
-        {loading ? (
-          <LoadingBox></LoadingBox>
-        ) : error ? (
-          <MessageBox variant="danger">{error}</MessageBox>
-        ) : (
-          <>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <>
           {products.length === 0 && <MessageBox>No Products Found</MessageBox>}
-          <div className="row center">
+          <ProductWrapper>
             {products.map((product) => (
               <Product key={product._id} product={product}></Product>
             ))}
-          </div>
-          </>
-        )}  
-    </div>);
+          </ProductWrapper>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default HomeScreen;
