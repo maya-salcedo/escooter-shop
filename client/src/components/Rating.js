0a3ee5aea3b5c43048f3d49aa48027ed55958b1d
maya <maya.salcedo07@gmail.com>
2021-08-10 21:46:46 +0300

    added styled componet in rating

--- client/src/components/Rating.js
+++ client/src/components/Rating.js
@@ -1,59 +1,82 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  > span {
+    color: #f0c040;
+    margin: 0.1rem;
+  }
+  span:last-child {
+    color: #403f48;
+  }
+`;
 
 const Rating = (props) => {
   const { rating, numReviews, caption } = props;
-  return(
-    <div className="rating">
-          <span>
-            <i className={
-              rating >= 1 
-                ? "fa fa-star"
-                : rating >= 0.5
-                ? "fa fa-star-half-o"
-                : "fa fa-o"}
-            ></i>
-          </span>
-          <span>
-            <i className={
-              rating >= 2 
-                ? "fa fa-star"
-                : rating >= 1.5
-                ? "fa fa-star-half-o"
-                : "fa fa-o"}
-            ></i>
-          </span>
-          <span>
-            <i className={
-              rating >= 3 
-                ? "fa fa-star"
-                : rating >= 2.5
-                ? "fa fa-star-half-o"
-                : "fa fa-o"}
-            ></i>
-          </span>
-          <span>
-            <i className={
-              rating >= 4 
-                ? "fa fa-star"
-                : rating >= 3.5
-                ? "fa fa-star-half-o"
-                : "fa fa-o"}
-            ></i>
-          </span>
-          <span>
-            <i className={
-              rating >= 5 
-                ? "fa fa-star"
-                : rating >= 4.5
-                ? "fa fa-star-half-o"
-                : "fa fa-o"}
-            ></i>
-          </span>
-          {caption ? (<span>{caption}</span>) 
-          : ( <span>{numReviews + ' reviews'}</span>)
-          }    
-        </div>
+  return (
+    <Wrapper>
+      <span>
+        <i
+          className={
+            rating >= 1
+              ? 'fa fa-star'
+              : rating >= 0.5
+              ? 'fa fa-star-half-o'
+              : 'fa fa-o'
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          className={
+            rating >= 2
+              ? 'fa fa-star'
+              : rating >= 1.5
+              ? 'fa fa-star-half-o'
+              : 'fa fa-o'
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          className={
+            rating >= 3
+              ? 'fa fa-star'
+              : rating >= 2.5
+              ? 'fa fa-star-half-o'
+              : 'fa fa-o'
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          className={
+            rating >= 4
+              ? 'fa fa-star'
+              : rating >= 3.5
+              ? 'fa fa-star-half-o'
+              : 'fa fa-o'
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          className={
+            rating >= 5
+              ? 'fa fa-star'
+              : rating >= 4.5
+              ? 'fa fa-star-half-o'
+              : 'fa fa-o'
+          }
+        ></i>
+      </span>
+      {caption ? (
+        <span>{caption}</span>
+      ) : (
+        <span>{numReviews + ' reviews'}</span>
+      )}
+    </Wrapper>
   );
-}
+};
 
 export default Rating;
