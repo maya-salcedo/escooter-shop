22247374564e0890c8eb6ff26d545e54bf93da04
maya <maya.salcedo07@gmail.com>
2021-08-10 15:03:04 +0300

    applied order screen and form wrapper components to product screen

--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -6,7 +6,12 @@ import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 import Rating from '../components/Rating';
 import { PRODUCT_REVIEW_CREATE_RESET } from '../constants/productConstants';
-
+import FormWrapper from '../elements/FormWrapper ';
+import OrderScreenWrapper, {
+  DetailWrapper,
+  ImageWrapper,
+} from '../elements/OrderScreenWrapper';
+import YellowButtonWrapper from '../elements/YellowButtonWrapper';
 
 const ProductScreen = (props) => {
   const dispatch = useDispatch();
@@ -18,7 +23,11 @@ const ProductScreen = (props) => {
   const { userInfo } = userSignin;
 
   const productReviewCreate = useSelector((state) => state.productReviewCreate);
-  const { loading: loadingReviewCreate, error: errorReviewCreate, success: successReviewCreate } = productReviewCreate;
+  const {
+    loading: loadingReviewCreate,
+    error: errorReviewCreate,
+    success: successReviewCreate,
+  } = productReviewCreate;
 
   const [rating, setRating] = useState(0);
   const [comment, setComment] = useState('');
@@ -35,18 +44,20 @@ const ProductScreen = (props) => {
 
   const addToCartHandler = () => {
     props.history.push(`/cart/${productId}?qty=${qty}`);
-  }
+  };
 
   const submitHandler = (e) => {
     e.preventDefault();
     if (comment && rating) {
-      dispatch(createReview(productId, { rating, comment, name: userInfo.name}));
+      dispatch(
+        createReview(productId, { rating, comment, name: userInfo.name })
+      );
     } else {
       alert('Please enter comment and rating');
     }
-  }
+  };
 
-  return(
+  return (
     <div>
       {loading ? (
         <LoadingBox></LoadingBox>
@@ -55,11 +66,14 @@ const ProductScreen = (props) => {
       ) : (
         <div>
           <Link to="/">Back to results</Link>
-          <div className="row top">
-            <div className="col-2">
-              <img className="large" src={product.image} alt={product.name}></img>
+          <OrderScreenWrapper>
+            <div className="column2">
+              <ImageWrapper
+                src={product.image}
+                alt={product.name}
+              ></ImageWrapper>
             </div>
-            <div className="col-1">
+            <div className="column1">
               <ul>
                 <li>
                   <h1>{product.name}</h1>
@@ -70,18 +84,22 @@ const ProductScreen = (props) => {
                     numReviews={product.numReviews}
                   ></Rating>
                 </li>
-                <li>Price : € {product.price}</li>
                 <li>
-                  Description:
-                  <p>{product.description}</p>
+                  <b>Price :</b> € {product.price}
+                </li>
+                <li>
+                  <b>Description:</b>
+                  <p>
+                    <i>{product.description}</i>
+                  </p>
                 </li>
               </ul>
             </div>
-            <div className="col-1">
-              <div className="card card-body">
+            <div className="column1">
+              <div className="container">
                 <ul>
                   <li>
-                    Seller{' '}
+                    Seller
                     <h2>
                       <Link to={`/seller/${product.seller._id}`}>
                         {product.seller.seller.name}
@@ -93,98 +111,117 @@ const ProductScreen = (props) => {
                     ></Rating>
                   </li>
                   <li>
-                    <div className="row">
+                    <DetailWrapper>
                       <div>Price</div>
                       <div className="price">€{product.price}</div>
-                    </div>
+                    </DetailWrapper>
                   </li>
                   <li>
-                    <div className="row">
+                    <DetailWrapper>
                       <div>Status</div>
                       <div>
-                        {product.countInStock > 0 ? (<span className="success">In stock</span>
+                        {product.countInStock > 0 ? (
+                          <span className="success">In stock</span>
                         ) : (
                           <span className="danger">Unavailable</span>
                         )}
                       </div>
-                    </div>
+                    </DetailWrapper>
                   </li>
                   {product.countInStock > 0 && (
                     <>
-                    <li>
-                      <div className="row">
-                        <div>Qty</div>
-                        <div>
-                          <select
-                            value={qty}
-                            onChange={(e) => setQty(e.target.value)}
-                          >
-                            {[...Array(product.countInStock).keys()].map(
-                              (x) => ( 
-                                <option key={x + 1} value={x + 1}>{x + 1}</option>
-                              )
-                            )}
-                          </select>
-                        </div>
-                      </div>
-                    </li>
-                    <li>
-                     <button 
-                        className="primary block"
-                        onClick={addToCartHandler}
-                      >
-                        Add to Cart
-                      </button>
-                    </li>
+                      <li>
+                        <DetailWrapper>
+                          <div>Qty</div>
+                          <div>
+                            <select
+                              value={qty}
+                              onChange={(e) => setQty(e.target.value)}
+                            >
+                              {[...Array(product.countInStock).keys()].map(
+                                (x) => (
+                                  <option key={x + 1} value={x + 1}>
+                                    {x + 1}
+                                  </option>
+                                )
+                              )}
+                            </select>
+                          </div>
+                        </DetailWrapper>
+                      </li>
+                      <li>
+                        <YellowButtonWrapper
+                          width="100%"
+                          onClick={addToCartHandler}
+                          text="Add to Cart"
+                        ></YellowButtonWrapper>
+                      </li>
                     </>
                   )}
                 </ul>
               </div>
             </div>
-          </div>
+          </OrderScreenWrapper>
           <div>
             <h2 id="reviews">Reviews</h2>
-            {product.reviews.length === 0 && (<MessageBox>There is no review</MessageBox>)}
+            {product.reviews.length === 0 && (
+              <MessageBox>There is no review</MessageBox>
+            )}
             <ul>
-              {product.reviews.map((review) => (
-                <li key={review._id}>
-                  <strong>{review.name}</strong>
-                  <Rating rating={review.rating} caption=" "></Rating>
-                  <p>{review.createdAt.substring(0,10)}</p>
-                  <p>{review.comment}</p>
-                </li>
-              ))}
+              <li>
+                <div>
+                  {product.reviews.map((review) => (
+                    <div key={review._id}>
+                      <strong>{review.name}</strong>
+                      <Rating rating={review.rating} caption=" "></Rating>
+                      <p>{review.createdAt.substring(0, 10)}</p>
+                      <p>{review.comment}</p>
+                    </div>
+                  ))}
+                </div>
+              </li>
               <li>
                 {userInfo ? (
-                  <form className="form" onSubmit={submitHandler}>
+                  <FormWrapper onSubmit={submitHandler}>
                     <div>
                       <h2>Write a customer review</h2>
                     </div>
                     <div>
                       <label htmlFor="rating">Rating</label>
-                      <select id="rating" value={rating}
-                        onChange={(e) => setRating(e.target.value)}>
-                          <option value="">Select</option>
-                          <option value="1">1- Poor</option>
-                          <option value="2">2- Fair</option>
-                          <option value="3">3- Good</option>
-                          <option value="4">4- Very good</option>
-                          <option value="4">5- Excellent</option>
+                      <select
+                        id="rating"
+                        value={rating}
+                        onChange={(e) => setRating(e.target.value)}
+                      >
+                        <option value="">Select</option>
+                        <option value="1">1- Poor</option>
+                        <option value="2">2- Fair</option>
+                        <option value="3">3- Good</option>
+                        <option value="4">4- Very good</option>
+                        <option value="4">5- Excellent</option>
                       </select>
                     </div>
                     <div>
                       <label htmlFor="comment">Comment</label>
-                      <textarea id="comment" value={comment} onChange={(e) => setComment(e.target.value)}></textarea>
+                      <textarea
+                        id="comment"
+                        value={comment}
+                        onChange={(e) => setComment(e.target.value)}
+                      ></textarea>
                     </div>
                     <div>
                       <label />
-                      <button className="primary" type="submit">Submit</button>
+                      <button type="submit">Submit</button>
                     </div>
                     <div>
                       {loadingReviewCreate && <LoadingBox></LoadingBox>}
-                      {errorReviewCreate && <MessageBox variant="danger">{errorReviewCreate}</MessageBox>}
+                      {errorReviewCreate && (
+                        <MessageBox variant="danger">
+                          {errorReviewCreate}
+                        </MessageBox>
+                      )}
                     </div>
-                  </form>
+                  </FormWrapper>
                 ) : (
                   <MessageBox>
                     Please <Link to="/signin">Sign In</Link> to write a review
@@ -194,10 +231,9 @@ const ProductScreen = (props) => {
             </ul>
           </div>
         </div>
-        )}  
+      )}
     </div>
-    
   );
-}
+};
 
 export default ProductScreen;
